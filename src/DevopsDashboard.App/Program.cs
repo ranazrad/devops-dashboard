14a778528bafc2fa10e5c2c278ab584fbf28aad8
Ran Azrad <ranazrad@gmail.com>
2026-05-23 11:55:30 +0300

    Update Program.cs

--- src/DevopsDashboard.App/Program.cs
+++ src/DevopsDashboard.App/Program.cs
@@ -1,1 +1,6 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<DevopsDashboard.App.App>("#app");
+
+await builder.Build().RunAsync();
